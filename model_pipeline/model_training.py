cf214c3fdbca0e19eebf4a7be9b744e89fa00433
pphanhh <11220655@st.neu.edu.vn>
2025-05-07 12:30:37 +0700

    Clean tracked files that should be ignored

--- model_pipeline/model_training.py
+++
@@ -1,5 +0,0 @@
-# model_training.py: trains new models using MLflow and logs the model.
-# This script includes loading data from feature_store in Postgre database
-# and training a model using different algorithms.
-# Input: Postgre database file containing features.
-# Output: MLflow model logged to the tracking server.
